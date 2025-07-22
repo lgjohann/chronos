d7ba03fd26566423208c90a24a24c33d9a5d5a94
Guilherme Johann <guilherme4695@gmail.com>
2025-07-22 16:10:40 -0300

    tá no meio do desenvolvimento da bagaça já

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,60 @@
+import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
+import styles from './styles.module.css';
+import { useState } from 'react';
+
+type AvailableThemes = 'dark' | 'light';
+
+export function Menu() {
+  const [theme, setTheme] = useState<AvailableThemes>('dark');
+
+  function handleThemeChange(
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  ) {
+    event.preventDefault();
+    setTheme(prevTheme => {
+      const nextTheme = prevTheme === 'dark' ? 'light' : 'dark';
+      return nextTheme;
+    });
+  }
+
+  return (
+    <nav className={styles.menu}>
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Ir para a Home'
+        title='Ir para a Home'
+      >
+        <HouseIcon />
+      </a>
+
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Ver o Histórico'
+        title='Ver o Histórico'
+      >
+        <HistoryIcon />
+      </a>
+
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Configurações'
+        title='Configurações'
+      >
+        <SettingsIcon />
+      </a>
+
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Mudar o Tema'
+        title='Mudar o Tema'
+        onClick={handleThemeChange}
+      >
+        <SunIcon />
+      </a>
+    </nav>
+  );
+}

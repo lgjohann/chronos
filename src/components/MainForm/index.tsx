4d8a0eb3d360de787ed62c9da944e05f00be07fd
Guilherme Johann <guilherme4695@gmail.com>
2025-07-24 10:01:26 -0300

    iniciando state no input pra controlar

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -4,11 +4,13 @@ import { PlayCircleIcon } from 'lucide-react';
 import { Cycles } from '../Cycles';
 import { DefaultButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
+import { useState } from 'react';
 
 export function MainForm() {
+  const [taskName, setTaskName] = useState('');
+
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    console.log('Nova tarefa criada');
   }
 
   return (
@@ -19,6 +21,8 @@ export function MainForm() {
           id='taskInput'
           type='text'
           placeholder='Digite algo...'
+          value={taskName}
+          onChange={e => setTaskName(e.target.value)}
         />
       </div>
 
